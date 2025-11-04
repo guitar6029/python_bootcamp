c1264966dd1169dbf1a4f86c911cc2d49a21a155
Josh <jsbusiness305@gmail.com>
2025-11-04 09:37:25 -0500

    basics

---
+++ numbers.py
@@ -0,0 +1,23 @@
+### basic maths
+
+#addition
+print(1+1)
+
+#subtraction
+print(1-1)
+
+#multiplication
+print(1*1)
+
+#division
+print(1/1)
+
+#modulus
+# this returns the remainder
+print(50%5)
+
+# check if a number is even
+print(50%2 == 0)
+
+#power 2 to the power of 5
+print(2**5)
