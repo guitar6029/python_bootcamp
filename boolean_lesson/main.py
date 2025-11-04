419ca5ebdef226a5e2525220410a1758cade898e
Josh <jsbusiness305@gmail.com>
2025-11-04 13:41:23 -0500

    added examples

---
+++ boolean_lesson/main.py
@@ -0,0 +1,9 @@
+# boolean
+wascalculated = False
+print(wascalculated)
+
+minvalue = 5
+maxvalue = 100
+
+print(f' minvalue is less than maxvalue {minvalue < maxvalue}')
+print((minvalue < maxvalue) != wascalculated)
