c4a71d07db61148c549a6cf77edbf36343950692
Josh <jsbusiness305@gmail.com>
2025-11-04 13:35:20 -0500

    added examples

---
+++ set_lesson/main.py
@@ -0,0 +1,10 @@
+# set example
+# is it an unordered, mutalble collection of unique items
+
+myset = set(["1", "2", "3", "3", "1", "6"])
+myset.add("3")
+
+print(myset)
+
+#order set
+print(sorted(myset))
