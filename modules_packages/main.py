1627212854757dbb7cb3924c174b0d05c80f7d91
jsdev305 <jsdev305@gmail.com>
2025-11-18 10:34:48 -0500

    added examples for __name__

--- modules_packages/main.py
+++ modules_packages/main.py
@@ -1,6 +1,9 @@
 from mymodule import hello
 from sub1.mymodule import greet
+from one import myfunc
 
 hello()
 
 greet('James')
+
+myfunc()
---
+++ modules_packages/one.py
@@ -0,0 +1,7 @@
+print('hello')
+
+def myfunc():
+    print('my function is called inside one.py')
+
+if __name__ == "__main__":
+    myfunc()
